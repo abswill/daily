3cebaa9e9177403b02a62257b846a0843e5a00c9
zhangjinzhen <415952948@qq.com>
2020-03-08 11:29:16 +0800

    feat: lodash 第一讲

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/daily01">daily01</router-link> |
-      <router-link to="/element01-cbutton">element01</router-link>
+      <!-- <router-link to="/">Home</router-link> | -->
+      <router-link to="/daily01">daily01</router-link>
+      <!-- <router-link to="/element01-cbutton">element01</router-link> -->
     </div>
     <router-view/>
   </div>
